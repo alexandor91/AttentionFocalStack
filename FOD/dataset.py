3290a9ffc343ef584fc03c5f55f13f56b97052fd
Alexander.Kang <kangxueyang@126.com>
2025-10-17 20:16:48 +1100

    Update dataset.py, with support of focal stack images

--- FOD/dataset.py
+++ FOD/dataset.py
@@ -15,6 +15,7 @@ import torchvision.transforms.functional as TF
 
 from FOD.api import get_total_paths, get_splitted_dataset, get_transforms
 
+
 def show(imgs):
     fix, axs = plt.subplots(ncols=len(imgs), squeeze=False)
     for i, img in enumerate(imgs):
@@ -23,106 +24,135 @@ def show(imgs):
         axs[0, i].set(xticklabels=[], yticklabels=[], xticks=[], yticks=[])
     plt.show()
 
-class AutoFocusDataset(Dataset):
+
+class AutoFocusStackDataset(Dataset):
     """
-        Dataset class for the AutoFocus Task. Requires for each image, its depth ground-truth and
-        segmentation mask
-        Args:
-            :- config -: json config file
-            :- dataset_name -: str
-            :- split -: split ['train', 'val', 'test']
+    Dataset for focal stack inputs.
+    Each sample = [stack of focus images], depth GT.
+    Directory format expected:
+        path_images/
+            scene_0001/
+                focus_00.png
+                focus_01.png
+                ...
+            scene_0002/
+                ...
+        path_depths/
+            scene_0001.png
+            scene_0002.png
     """
+
     def __init__(self, config, dataset_name, split=None):
         self.split = split
         self.config = config
 
-        path_images = os.path.join(config['Dataset']['paths']['path_dataset'], dataset_name, config['Dataset']['paths']['path_images']) #config['Dataset']['paths']['path_images']
-        path_depths = os.path.join(config['Dataset']['paths']['path_dataset'], dataset_name, config['Dataset']['paths']['path_depths']) #config['Dataset']['paths']['path_depths']
-        #path_segmentations = os.path.join(config['Dataset']['paths']['path_dataset'], dataset_name, config['Dataset']['paths']['path_segmentations'])
-        print('########################')
-        print(path_images)
-        print(path_depths)
+        base_path = config['Dataset']['paths']['path_dataset']
+        path_images = os.path.join(base_path, dataset_name, config['Dataset']['paths']['path_images'])
+        path_depths = os.path.join(base_path, dataset_name, config['Dataset']['paths']['path_depths'])
 
-        self.paths_images = get_total_paths(path_images, config['Dataset']['extensions']['ext_images'])
+        print('########################')
+        print("Image path:", path_images)
+        print("Depth path:", path_depths)
+
+        # list of subfolders (each contains one focal stack)
+        self.stack_dirs = sorted(
+            [d for d in glob(os.path.join(path_images, '*')) if os.path.isdir(d)]
+        )
+
         self.paths_depths = get_total_paths(path_depths, config['Dataset']['extensions']['ext_depths'])
-        #self.paths_segmentations = get_total_paths(path_segmentations, config['Dataset']['extensions']['ext_segmentations'])
 
         assert (self.split in ['train', 'test', 'val']), "Invalid split!"
-        assert (len(self.paths_images) == len(self.paths_depths)), "Different number of instances between the input and the depth maps"
-        #assert (len(self.paths_images) == len(self.paths_segmentations)), "Different number of instances between the input and the segmentation maps"
-        assert (config['Dataset']['splits']['split_train']+config['Dataset']['splits']['split_test']+config['Dataset']['splits']['split_val'] == 1), "Invalid splits (sum must be equal to 1)"
-        # check for segmentation
+        assert (len(self.stack_dirs) == len(self.paths_depths)), (
+            f"Different number of stacks ({len(self.stack_dirs)}) and depths ({len(self.paths_depths)})"
+        )
+        assert (
+            config['Dataset']['splits']['split_train']
+            + config['Dataset']['splits']['split_test']
+            + config['Dataset']['splits']['split_val']
+            == 1
+        ), "Invalid splits (sum must be 1)"
 
-        # utility func for splitting
-        self.paths_images, self.paths_depths = get_splitted_dataset(config, self.split, dataset_name, self.paths_images, self.paths_depths)
+        # split into train/val/test subsets
+        self.stack_dirs, self.paths_depths = get_splitted_dataset(
+            config, self.split, dataset_name, self.stack_dirs, self.paths_depths
+        )
 
-        # Get the transforms
+        # Get transforms
         self.transform_image, self.transform_depth = get_transforms(config)
 
-        # get p_flip from config
-        self.p_flip = config['Dataset']['transforms']['p_flip'] if split=='train' else 0
-        self.p_crop = config['Dataset']['transforms']['p_crop'] if split=='train' else 0
-        self.p_rot = config['Dataset']['transforms']['p_rot'] if split=='train' else 0
+        # probabilities for augmentations
+        self.p_flip = config['Dataset']['transforms']['p_flip'] if split == 'train' else 0
+        self.p_crop = config['Dataset']['transforms']['p_crop'] if split == 'train' else 0
+        self.p_rot = config['Dataset']['transforms']['p_rot'] if split == 'train' else 0
         self.resize = config['Dataset']['transforms']['resize']
 
     def __len__(self):
-        """
-            Function to get the number of images using the given list of images
-        """
-        return len(self.paths_images)
+        return len(self.stack_dirs)
+
+    def _load_stack(self, stack_dir):
+        """Load all focal images in sorted order from a given stack directory"""
+        img_paths = sorted(glob(os.path.join(stack_dir, '*')))
+        stack = []
+        for p in img_paths:
+            img = Image.open(p)
+            img_t = self.transform_image(img)
+            stack.append(img_t)
+        # stack into a single tensor [N, C, H, W]
+        return torch.stack(stack, dim=0)
 
     def __getitem__(self, idx):
-        """
-            Getter function in order to get the triplet of images / depth maps and segmentation masks
-        """
-        print("iteration begins!")
         if torch.is_tensor(idx):
             idx = idx.tolist()
-        image = self.transform_image(Image.open(self.paths_images[idx]))
-        print("color image ######")
+
+        # Load all focus images
+        img_stack = self._load_stack(self.stack_dirs[idx])
+
+        # Load depth GT
         depth = self.transform_depth(Image.open(self.paths_depths[idx]))
-        print("depth image")
 
-        #segmentation = self.transform_seg(Image.open(self.paths_segmentations[idx]))
-        imgorig = image.clone()
-
+        # --- Data augmentation applied consistently ---
         if random.random() < self.p_flip:
-            image = TF.hflip(image)
+            img_stack = torch.flip(img_stack, dims=[3])  # flip W dimension
             depth = TF.hflip(depth)
-            #segmentation = TF.hflip(segmentation)
 
         if random.random() < self.p_crop:
-            random_size = random.randint(256, self.resize-1)
+            random_size = random.randint(256, self.resize - 1)
             max_size = self.resize - random_size
-            left = int(random.random()*max_size)
-            top = int(random.random()*max_size)
-            image = TF.crop(image, top, left, random_size, random_size)
+            left = int(random.random() * max_size)
+            top = int(random.random() * max_size)
+
+            img_stack = img_stack[:, :, top:top + random_size, left:left + random_size]
             depth = TF.crop(depth, top, left, random_size, random_size)
-            #segmentation = TF.crop(segmentation, top, left, random_size, random_size)
-            image = transforms.Resize((self.resize, self.resize))(image)
+
+            img_stack = torch.stack([
+                transforms.Resize((self.resize, self.resize))(img)
+                for img in img_stack
+            ])
             depth = transforms.Resize((self.resize, self.resize))(depth)
-            #segmentation = transforms.Resize((self.resize, self.resize), interpolation=transforms.InterpolationMode.NEAREST)(segmentation)
 
         if random.random() < self.p_rot:
-            #rotate
-            random_angle = random.random()*20 - 10 #[-10 ; 10]
-            mask = torch.ones((1,self.resize,self.resize)) #useful for the resize at the end
+            random_angle = random.random() * 20 - 10  # [-10, 10]
+            mask = torch.ones((1, self.resize, self.resize))
             mask = TF.rotate(mask, random_angle, interpolation=transforms.InterpolationMode.BILINEAR)
-            image = TF.rotate(image, random_angle, interpolation=transforms.InterpolationMode.BILINEAR)
+
+            img_stack = torch.stack([
+                TF.rotate(img, random_angle, interpolation=transforms.InterpolationMode.BILINEAR)
+                for img in img_stack
+            ])
             depth = TF.rotate(depth, random_angle, interpolation=transforms.InterpolationMode.BILINEAR)
-            #segmentation = TF.rotate(segmentation, random_angle, interpolation=transforms.InterpolationMode.NEAREST)
-            #crop to remove black borders due to the rotation
-            left = torch.argmax(mask[:,0,:]).item()
-            top = torch.argmax(mask[:,:,0]).item()
-            coin = min(left,top)
-            size = self.resize - 2*coin
-            image = TF.crop(image, coin, coin, size, size)
+
+            left = torch.argmax(mask[:, 0, :]).item()
+            top = torch.argmax(mask[:, :, 0]).item()
+            coin = min(left, top)
+            size = self.resize - 2 * coin
+
+            img_stack = img_stack[:, :, coin:coin + size, coin:coin + size]
             depth = TF.crop(depth, coin, coin, size, size)
-            #segmentation = TF.crop(segmentation, coin, coin, size, size)
-            #Resize
-            image = transforms.Resize((self.resize, self.resize))(image)
+
+            img_stack = torch.stack([
+                transforms.Resize((self.resize, self.resize))(img)
+                for img in img_stack
+            ])
             depth = transforms.Resize((self.resize, self.resize))(depth)
-            #segmentation = transforms.Resize((self.resize, self.resize), interpolation=transforms.InterpolationMode.NEAREST)(segmentation)
-        # show([imgorig, image, depth, segmentation])
-        # exit(0)
-        return image, depth
+
+        return img_stack, depth
